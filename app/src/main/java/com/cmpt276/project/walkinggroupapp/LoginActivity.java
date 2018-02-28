9732994d9c996db8a312af8fa96375505be7cdcc
glang <glangbis@sfu.ca>
2018-02-27 19:09:05 -0800

    Wire Up Buttons/textViews/editText
    finish wiring up all components to appropriate listeners etc.

--- app/src/main/java/com/cmpt276/project/walkinggroupapp/LoginActivity.java
+++ app/src/main/java/com/cmpt276/project/walkinggroupapp/LoginActivity.java
@@ -2,10 +2,13 @@ package com.cmpt276.project.walkinggroupapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -27,6 +30,11 @@ public class LoginActivity extends AppCompatActivity {
         RegisterViews();
     }
 
+
+
+
+
+
     private void RegisterViews() {
 
         mLoginButton = findViewById(R.id.gerry_Login_Button_login);
@@ -42,6 +50,61 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //nothing for now
+            }
+        });
+
+        mForgotPasswordTextView = findViewById(R.id.gerry_ForgotPassword_TextView_login);
+        mForgotPasswordTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //go to ForgotPassword Activity
+                Toast.makeText(LoginActivity.this,"click succes",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        mPasswordEditText = findViewById(R.id.gerry_Password_EditText_login);
+        mPasswordEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                if(s.length() != 0) {
+
+                    mPassword = s.toString();
+                }
+
+            }
+        });
+
+        mEmailEditText = findViewById(R.id.gerry_Email_EditText_login);
+        mEmailEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                if(s.length() != 0) {
+
+                    mEmail = s.toString();
+                }
             }
         });
 
