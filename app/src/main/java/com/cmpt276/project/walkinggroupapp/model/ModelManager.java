ae9a6111bb7101a718f3779ebc4798ad81fb622a
zherenx <zherenx@sfu.ca>
2018-03-03 16:41:28 -0800

    Create required member field for JSON deserialization.
    Create Group class.
    
    Create UserManager class.
    
    Create ModelManager class.

---
+++ app/src/main/java/com/cmpt276/project/walkinggroupapp/model/ModelManager.java
@@ -0,0 +1,8 @@
+package com.cmpt276.project.walkinggroupapp.model;
+
+/**
+ * Created by zherenx on 2018-03-03.
+ */
+
+public class ModelManager {
+}
--- app/src/main/java/com/cmpt276/project/walkinggroupapp/model/User.java
+++ app/src/main/java/com/cmpt276/project/walkinggroupapp/model/User.java
@@ -1,8 +1,51 @@
 package com.cmpt276.project.walkinggroupapp.model;
 
-/**
- * Created by zherenx on 2018-02-27.
- */
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * This class model a user account.
+ */
 public class User {
+
+    private long id;
+
+    private String name;
+    private String emailAddress;
+
+    // TODO: is password needed here?
+    //private String password;
+
+    // TODO: list of User or String (emailAddress);
+    private List<User> monitoredByUsers;
+    private List<User> monitorsUsers;
+
+    private List<Group> walkingGroups;
+
+    // TODO: need a new name;
+    private String href;
+
+
+//    public User(String name, String emailAddress, String password) {
+//        this.name = name;
+//        this.emailAddress = emailAddress;
+//
+//        this.password = password;
+//
+//        monitorsUsers = new ArrayList<>();
+//        monitoredByUsers = new ArrayList<>();
+//    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void addMonitoringUser(String emailAddress) {
+
+    }
+
 }
