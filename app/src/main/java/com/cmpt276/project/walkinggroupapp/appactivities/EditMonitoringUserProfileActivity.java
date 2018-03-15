3b817a0ff77b32b21228e9dee62d9e0111330421
jktse <21jackytse@gmail.com>
2018-03-14 22:37:05 -0700

    Extracted string resources, and added a few comments regarding code that I used in Assignment 2

--- app/src/main/java/com/cmpt276/project/walkinggroupapp/appactivities/EditMonitoringUserProfileActivity.java
+++ app/src/main/java/com/cmpt276/project/walkinggroupapp/appactivities/EditMonitoringUserProfileActivity.java
@@ -26,6 +26,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+/******
+ *  Notice the complex List Adapter is based off from https://www.youtube.com/watch?v=WRANgDgM2Zg
+ *  a video provided by the prof for assignment 2
+ *  Code is adapted from Jacky.T  Assignment 2
+ */
+
 public class EditMonitoringUserProfileActivity extends AppCompatActivity {
 
     public static final String USER_ID = "UserID";
