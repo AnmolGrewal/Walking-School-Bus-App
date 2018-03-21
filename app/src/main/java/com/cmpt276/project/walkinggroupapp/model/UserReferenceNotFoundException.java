f59427cf25a82784c5a32f4955c2819297d54f28
zherenx <zherenx@sfu.ca>
2018-03-20 17:43:27 -0700

    Delete UserManger and UserReferenceNotFoundException in model.

--- app/src/main/java/com/cmpt276/project/walkinggroupapp/model/UserReferenceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.cmpt276.project.walkinggroupapp.model;
-
-class UserReferenceNotFoundException extends Exception {
-    public UserReferenceNotFoundException(String message) {
-        super(message);
-    }
-}
