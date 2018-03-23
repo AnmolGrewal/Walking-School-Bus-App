f3394826174969384c13eb91d9576a3f28359659
Anmo <anmolsgrewal@gmail.com>
2018-03-23 16:32:20 -0700

    Fixed Hardcoded Text
    Signed-off-by: Anmo <anmolsgrewal@gmail.com>

--- app/src/main/java/com/cmpt276/project/walkinggroupapp/RegisterActivityStudent.java
+++ app/src/main/java/com/cmpt276/project/walkinggroupapp/RegisterActivityStudent.java
@@ -14,7 +14,6 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.cmpt276.project.walkinggroupapp.appactivities.LoginActivity;
-import com.cmpt276.project.walkinggroupapp.appactivities.RegisterActivityParent;
 import com.cmpt276.project.walkinggroupapp.model.ModelManager;
 import com.cmpt276.project.walkinggroupapp.proxy.ProxyBuilder;
 
