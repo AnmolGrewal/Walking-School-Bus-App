b27b816a164c5ba60964500f6bdcdc3c2ebf80fc
jktse <21jackytse@gmail.com>
2018-03-22 21:39:58 -0700

    Fixed bug where address was being displayed rather than name,
    changed getAddress to getName

--- app/src/main/java/com/cmpt276/project/walkinggroupapp/appactivities/MainMenuActivity.java
+++ app/src/main/java/com/cmpt276/project/walkinggroupapp/appactivities/MainMenuActivity.java
@@ -182,7 +182,7 @@ public class MainMenuActivity extends AppCompatActivity {
 
             //Name:
             TextView makeName = itemView.findViewById(R.id.jacky_user_name_dynamic);
-            makeName.setText(currentUser.getAddress());
+            makeName.setText(currentUser.getName());
 
             //Email
             TextView makeEmail = itemView.findViewById(R.id.jacky_user_email_dynamic);
