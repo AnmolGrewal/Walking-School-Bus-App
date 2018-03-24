4027b961790feccb758f0300f994dd775092a04b
jktse <21jackytse@gmail.com>
2018-03-24 15:38:46 -0700

    Fixed a bug where if you recieved a long message it was getting cut off

--- app/src/main/java/com/cmpt276/project/walkinggroupapp/fragments/viewMessageFragment.java
+++ app/src/main/java/com/cmpt276/project/walkinggroupapp/fragments/viewMessageFragment.java
@@ -4,25 +4,35 @@ package com.cmpt276.project.walkinggroupapp.fragments;
         import android.app.Dialog;
         import android.content.DialogInterface;
         import android.os.Bundle;
-        import android.os.Message;
         import android.support.v7.app.AppCompatDialogFragment;
         import android.view.LayoutInflater;
         import android.view.View;
+        import android.widget.TextView;
+
         import com.cmpt276.project.walkinggroupapp.R;
+
+/***
+ * Fragment used to display sent messages to user
+ *
+ * It creates a small dialog box to allow the user to see the message that was sent to them
+ */
 
 public class viewMessageFragment extends AppCompatDialogFragment
 {
     String message;
+    TextView messageLocation;
     @Override
     public Dialog onCreateDialog(Bundle saveInstanceState)
     {
         Bundle variables = getArguments();
-         message = variables.getString("MessageForUser", "Error With Message");
+        message = variables.getString("MessageForUser", "Error With Message");
 
         //Create the activity to show
         View v = LayoutInflater.from(getActivity())
                 .inflate(R.layout.view_message_layout, null);
         //Create a Button Listener
+        messageLocation = v.findViewById(R.id.jacky_view_message_text_view);
+        messageLocation.setText(message);
         DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
@@ -35,7 +45,7 @@ public class viewMessageFragment extends AppCompatDialogFragment
         };
 
         return new AlertDialog.Builder(getActivity())
-                .setTitle(message)
+                .setTitle("Viewing Message")
                 .setView(v)
                 .setPositiveButton("Okay", listener)
                 .create();
