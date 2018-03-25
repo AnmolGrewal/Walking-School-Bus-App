2dcdf10fb6cecd5869e21a44f1b7fbe34d05a811
glang <glangbis@sfu.ca>
2018-03-25 13:52:13 -0700

    Updated Permission Prompt Behaviour
    Permission access on MapActivity not has the proper behaviour

--- app/src/main/java/com/cmpt276/project/walkinggroupapp/appactivities/GroupInformation.java
+++ app/src/main/java/com/cmpt276/project/walkinggroupapp/appactivities/GroupInformation.java
@@ -1,12 +1,12 @@
 package com.cmpt276.project.walkinggroupapp.appactivities;
 
+import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Handler;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,33 +17,28 @@ import com.cmpt276.project.walkinggroupapp.R;
 import com.cmpt276.project.walkinggroupapp.model.GpsLocation;
 import com.cmpt276.project.walkinggroupapp.model.ModelManager;
 import com.cmpt276.project.walkinggroupapp.proxy.ProxyBuilder;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationAvailability;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.model.LatLng;
 
 import java.sql.Timestamp;
 
-public class GroupInformation extends AppCompatActivity  {
+
+/**
+ * Code for obtaining current location based on: https://www.youtube.com/watch?v=QNb_3QKSmMk
+ *
+ * Activity for viewing Information about a group and also uploading current location to server
+ */
+public class GroupInformation extends AppCompatActivity {
     private static final String TAG = "GroupInfoActivity";
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
 
-
-    private GoogleApiClient mGoogleApiClient;
-
-    private Location mLastLocation;
-
     private ModelManager mModelManager;
+
+    private LocationManager mLocationManager;
 
     private Button mStartUploadButton;
     private Button mStopUploadButton;
 
-    private GpsLocation mObtainedServerLocation;
-
     private boolean mIsUpload;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +46,10 @@ public class GroupInformation extends AppCompatActivity  {
         setContentView(R.layout.activity_group_information);
 
         mModelManager = ModelManager.getInstance();
+
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        setupLocationListener();
 
         setupButtons();
     }
@@ -75,12 +74,49 @@ public class GroupInformation extends AppCompatActivity  {
                 mIsUpload = false;
 
             }
+
         });
     }
 
+    private void setupLocationListener() {
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+
+        //request for permission
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]
+                    {android.Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+            return;
+        }
+
+        //get location update every 10 seconds
+        mLocationManager.requestLocationUpdates("gps", 10000, 0, locationListener);
+
+    }
 
     private void getLastKnownLocation() {
-        GpsLocation currentLocation = new GpsLocation(); //= new GpsLocation(lat,lng,timestamp);
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        GpsLocation currentLocation = new GpsLocation(12.0,12.0,timestamp);
 
         Log.w(TAG, "getLocation()4");
         ProxyBuilder.SimpleCallback<GpsLocation> setLastKnownLocationCallback = serverPassedLocation ->setLastKnownLocationResponse(serverPassedLocation);
@@ -90,10 +126,8 @@ public class GroupInformation extends AppCompatActivity  {
 
 
     private void setLastKnownLocationResponse(GpsLocation gpsLocation) {
-        //can double check here to make sure the returned gpsLocation  was the same as the one you passed to server
-
-
-        Log.w(TAG, "SET LOCATION SUCCESS");
+        //double check here to make sure the returned gpsLocation  was the same as the one you passed to server
+        Log.w(TAG, "SET LOCATION SUCCESS, Recieved: " + gpsLocation.getLat() + " " + gpsLocation.getLng() + " " + gpsLocation.getTimestamp().toString());
 
         //wait 30 seconds and request for current location again
         Handler handler = new Handler();
