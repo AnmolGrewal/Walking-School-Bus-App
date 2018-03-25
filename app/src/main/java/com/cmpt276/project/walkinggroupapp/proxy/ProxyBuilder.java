1f4f60c778ccf11fbd2c79fd75da764186260430
zherenx <zherenx@sfu.ca>
2018-03-24 21:00:53 -0700

    Overload callProxy.

--- app/src/main/java/com/cmpt276/project/walkinggroupapp/proxy/ProxyBuilder.java
+++ app/src/main/java/com/cmpt276/project/walkinggroupapp/proxy/ProxyBuilder.java
@@ -105,6 +105,15 @@ public class ProxyBuilder {
      */
     public static <T extends Object> void callProxy(
             final Context context, Call<T> caller, final SimpleCallback<T> callback) {
+        callProxy(context, caller, callback, null);
+    }
+
+
+    public static <T extends Object> void callProxy(
+            final Context context,
+            Call<T> caller,
+            final SimpleCallback<T> onResponseCallback,
+            final SimpleCallback<T> onFailureCallback) {
         caller.enqueue(new Callback<T>() {
             @Override
             public void onResponse(Call<T> call, retrofit2.Response<T> response) {
@@ -122,9 +131,9 @@ public class ProxyBuilder {
                         }
                     }
 
-                    if (callback != null) {
+                    if (onResponseCallback != null) {
                         T body = response.body();
-                        callback.callback(body);
+                        onResponseCallback.callback(body);
                     }
                 } else {
                     String message;
@@ -143,6 +152,10 @@ public class ProxyBuilder {
             public void onFailure(Call<T> call, Throwable t) {
                 String message = "Server Error: " + t.getMessage();
                 showFailure(message);
+
+                if (onFailureCallback != null) {
+                    onFailureCallback.callback(null);
+                }
             }
             private void showFailure(String message) {
                 Log.e("ProxyBuilder", message);
@@ -152,6 +165,75 @@ public class ProxyBuilder {
             }
         });
     }
+
+
+
+
+
+    // This is the original copy of callProxy.
+// /**
+//     * Simplify the calling of the "Call"
+//     * - Handle error checking in one place and put up toast & log on failure.
+//     * - Callback to simplified interface on success.
+//     * @param context   Current activity for showing toast if there's an error.
+//     * @param caller    Call object returned by the proxy
+//     * @param callback  Client-code to execute when we have a good answer for them.
+//     * @param <T>       The type of data that Call object is expected to fetch
+//     */
+//    public static <T extends Object> void callProxy(
+//            final Context context, Call<T> caller, final SimpleCallback<T> callback) {
+//        caller.enqueue(new Callback<T>() {
+//            @Override
+//            public void onResponse(Call<T> call, retrofit2.Response<T> response) {
+//
+//                // Process the response
+//                if (response.errorBody() == null) {
+//                    // Check for authentication token:
+//                    String tokenInHeader = response.headers().get("Authorization");
+//                    if (tokenInHeader != null) {
+//                        if (receivedTokenCallback != null) {
+//                            receivedTokenCallback.callback(tokenInHeader);
+//                        } else {
+//                            // We got the token, but nobody wanted it!
+//                            Log.w("ProxyBuilder", "WARNING: Received token but no callback registered for it!");
+//                        }
+//                    }
+//
+//                    if (callback != null) {
+//                        T body = response.body();
+//                        callback.callback(body);
+//                    }
+//                } else {
+//                    String message;
+//                    try {
+//                        message = "CALL TO SERVER FAILED:\n" + response.errorBody().string();
+//                        //message = "Invalid Input";
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                        message = "Unable to decode response (body or error's body).";
+//                    }
+//                    showFailure(message);
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<T> call, Throwable t) {
+//                String message = "Server Error: " + t.getMessage();
+//                showFailure(message);
+//            }
+//            private void showFailure(String message) {
+//                Log.e("ProxyBuilder", message);
+//                if (context != null) {
+//                    Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+//                }
+//            }
+//        });
+//    }
+
+
+
+
+
 
 
 
