0bf45c9ae26d444e1f5920d47ed6069fb8bde9bc
zherenx <zherenx@sfu.ca>
2018-04-05 22:41:27 -0700

    Update User class;
    Create CustomReward class.

---
+++ app/src/main/java/com/cmpt276/project/walkinggroupapp/model/CustomReward.java
@@ -0,0 +1,18 @@
+package com.cmpt276.project.walkinggroupapp.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by zherenx on 2018-04-05.
+ */
+
+public class CustomReward {
+
+    // TODO: need to change name.
+
+    private Integer currentAvatar;
+
+    private List<Integer> ownedAvatars = new ArrayList<>();
+
+}
