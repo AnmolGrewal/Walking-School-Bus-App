d6dec37fbeb38f71707f63513f9615f7eb7c6ad0
Anmo <anmolsgrewal@gmail.com>
2018-04-08 14:04:12 -0700

    LeaderBoardActivity Created

--- app/src/main/java/com/cmpt276/project/walkinggroupapp/appactivities/LeaderBoardActivity.java
+++ app/src/main/java/com/cmpt276/project/walkinggroupapp/appactivities/LeaderBoardActivity.java
@@ -6,13 +6,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.cmpt276.project.walkinggroupapp.R;
+import com.cmpt276.project.walkinggroupapp.model.ModelManager;
 
 public class LeaderBoardActivity extends AppCompatActivity {
+
+    private ModelManager modelManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leader_board);
+
+        setupList();
+    }
+
+    private void setupList() {
+        
     }
 
     public static Intent makeIntent(Context context)
